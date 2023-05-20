73483745bf75ec32ff3a68d3e92d7f13862532c1
vilasjoshi <vilasjoshi222@gmail.com>
2023-05-20 22:59:15 +0530

    updated html,css, js. Added theme support. Device friendly update

--- script/main.js
+++ script/main.js
@@ -1,1 +1,15 @@
-console.log("Running...");
+const container = document.getElementById('container');
+const prefersDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+
+// Get the theme toggle button element
+const themeToggle = document.getElementById('theme-toggle');
+
+// Add event listener to toggle the theme on button click
+themeToggle.addEventListener('click', function() {
+  // Get the body element
+  const body = document.body;
+  
+  // Toggle the 'dark' class on the body element
+  body.classList.toggle('dark');
+});
